1c0902f12de1996ae42ad76276e0e3399383f31d
nalane <nathaniel.patrick.lane@gmail.com>
2019-06-09 00:29:06 -0600

    Ammended parse type

--- src/Resources/Model.hs
+++ src/Resources/Model.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Resources.Model (
     Model, createModel, drawModel, destroyModel
@@ -34,22 +32,22 @@ data Model = Model {
 
 makeLenses ''Model
 
---nVertParser :: Int -> Parsec s u [GLfloat]
+nVertParser :: Int -> Parser [GLfloat]
 nVertParser n = mapM (\_ -> do
     spaces
     ap sign floating) [1..n]
 
---vertex2Parser :: Parsec s u (Vertex2 GLfloat)
+vertex2Parser :: Parser (Vertex2 GLfloat)
 vertex2Parser = do
     (x:y:_) <- nVertParser 2
     return $ Vertex2 x y
 
---vertex3Parser :: Parsec s u (Vertex3 GLfloat)
+vertex3Parser :: Parser (Vertex3 GLfloat)
 vertex3Parser = do
     (x:y:z:_) <- nVertParser 3
     return $ Vertex3 x y z
 
---materialParser :: Parsec s u Material
+materialParser :: Parser Material
 materialParser = do
     diffuse <- vertex3Parser
     specular <- vertex3Parser
@@ -57,23 +55,23 @@ materialParser = do
     i <- read <$> many1 digit
     return $ Material diffuse specular i
 
---textureParser :: Parsec s u String
+textureParser :: Parser String
 textureParser = many $ noneOf "\n"
 
---uvParser :: Parsec s u (Vertex2 GLfloat)
+uvParser :: Parser (Vertex2 GLfloat)
 uvParser = do
     index <- many1 digit
     spaces
     vertex2Parser
 
---mapParser :: Parsec s u (Int, Int)
+mapParser :: Parser (Int, Int)
 mapParser = do
     vertIndex <- read <$> many1 digit
     spaces
     uvIndex <- read <$> many1 digit
     return (vertIndex, uvIndex)
 
---helper :: Parsec s u a -> Parsec s u [a]
+helper :: Parser a -> Parser [a]
 helper p = do
     num <- read <$> many1 digit
     spaces
@@ -90,40 +88,6 @@ parser = do
     u <- helper uvParser
     a <- helper mapParser
     return (m, v, t, u, a)
-
-{-
-parseMats :: [String] -> ([Material], [String])
-parseMats (num:dat) = (worker mats, rest) where
-    n = read num :: Int
-    (mats:rest) = splitAt (n * 7) dat
-
-    diffuse (r:g:b:_) = Vertex3 (read r) (read g) (read b)
-    specular (r:g:b:_) = Vertex3 (read r) (read g) (read b)
-    mat (dr:dg:db:sr:sg:sb:i:_) =
-        Material (diffuse [dr, dg, db]) (specular [sr, sg, sb]) $ read i
-
-    worker [] = []
-    worker arr = let (m:r) = splitAt 7 arr in
-        mat m : worker r
-
-parseVerts :: [String] -> ([Vertex3 GLfloat], [String])
-parseVerts (num:dat) = (worker verts, rest) where
-    n = read num :: Int
-    (verts:rest) = splitAt (n * 3) dat
-    worker [] = []
-    worker (x:y:z:fin) = Vertex3 (read x) (read y) (read z) : worker fin
-
-parseTextures :: [String] -> ([String], [String])
-parseTextures (num:dat) = (texts, rest) where
-    n = read num :: Int
-    (texts:rest) = splitAt n dat
-
-parseFeyFile :: [String] -> Model
-parseFeyFile dat = t where
-    (mats, matRest) = parseMats dat
-    (verts, vertRest) = parseVerts matRest
-    (texts, textRest) = parseTextures vertRest
--}
 
 createModel :: FilePath -> IO Model
 createModel path = do
