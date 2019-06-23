2a03ad115fa3df7c7b89b30f7bcae2087681b697
nalane <nathaniel.patrick.lane@gmail.com>
2019-06-22 23:25:56 -0600

    Fixed the texture drawing issue

--- src/Resources/Texture.hs
+++ src/Resources/Texture.hs
@@ -28,7 +28,10 @@ createTexture path = do
     unsafeWith d $ \ptr ->
         GL.texImage2D GL.Texture2D GL.NoProxy 0 GL.RGBA8 (GL.TextureSize2D w h) 0 $
         GL.PixelData GL.RGBA GL.UnsignedByte ptr
-    --GL.texture GL.Texture2D $= GL.Enabled
+
+    GL.textureFilter GL.Texture2D $= ((GL.Linear', Nothing), GL.Linear')
+    GL.textureWrapMode GL.Texture2D GL.S $= (GL.Mirrored, GL.ClampToEdge)
+    GL.textureWrapMode GL.Texture2D GL.T $= (GL.Mirrored, GL.ClampToEdge)
 
     return $ Texture texObj
 
@@ -36,7 +39,6 @@ drawTexture :: Texture -> IO ()
 drawTexture (Texture texObj) = do
     GL.activeTexture $= GL.TextureUnit 1
     GL.textureBinding GL.Texture2D $= Just texObj
-    --GL.texture GL.Texture2D $= GL.Enabled
 
 destroyTexture :: Texture -> IO ()
 destroyTexture (Texture texObj) =
